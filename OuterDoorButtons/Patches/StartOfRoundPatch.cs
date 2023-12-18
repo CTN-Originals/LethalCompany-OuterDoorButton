b9cb25ef94402d314299e9fb4efd9bbc2c60a1a6
CTN <info@ctn-originals.com>
2023-12-18 02:34:57 +0100

    Base Functionality

--- OuterDoorButtons/Patches/StartOfRoundPatch.cs
+++ OuterDoorButtons/Patches/StartOfRoundPatch.cs
@@ -5,6 +5,7 @@ using TMPro;
 using GameNetcodeStuff;
 
 using OuterDoorButtons.Utilities;
+using UnityEngine.UI;
 
 namespace OuterDoorButtons.Patches
 {
@@ -22,14 +23,33 @@ namespace OuterDoorButtons.Patches
 		private static Transform OpenButton;
 		private static Transform CloseButton;
 		private static InteractTrigger OpenButtonTrigger;
-		private static InteractTrigger CloseButtonTrigger;
-		private static TextMeshProUGUI PanelMeter;
+		// private static InteractTrigger CloseButtonTrigger;
+		private static Transform PanelMeter;
+		private static TextMeshProUGUI PanelMeterText;
 
+		//? Trigger paths to turn of for better visibility (MeshRenderer.enabled = false)
+		private static string[] VisibleTriggerPathsOrbit = {
+			"Environment/HangarShip/ReverbTriggers/",
+			"Environment/HangarShip/ReverbTriggers/LeavingShipTriggers/HorizontalTriggers/",
+		};
+
+		//? Trigger paths to turn of for better visibility (MeshRenderer.enabled = false)
+		private static string[] VisibleTriggerPathsLanded = {
+			"Environment/NavMeshColliders/PlayerShipNavmesh/",
+			"Environment/ReverbTriggers (1)/WindTriggers/",
+		};
+
+		//? Path to a light to use for better lighting outside the ship for development
+		//_ Environment/HangarShip/ShipModels2b/ShipLightsPost/Floodlight1
 
 		[HarmonyPostfix]
 		[HarmonyPatch("Start")]
 		private static void StartPatch() {
 			Console.LogInfo($"StartOfRound.Start() called");
+
+			// //!! DEVELOPMENT ONLY
+			// DisableVisibleTriggers(VisibleTriggerPathsOrbit);
+
 			GetObjectReferences();
 			if (MonitorWall == null || OriginalDoorPanel == null) return;
 			CreateMonitorDoorPanel();
@@ -38,9 +58,55 @@ namespace OuterDoorButtons.Patches
 		[HarmonyPostfix]
 		[HarmonyPatch("Update")]
 		private static void UpdatePatch() {
-			if (OriginalPanelMeter == null || PanelMeter == null) return;
-			PanelMeter.SetText(OriginalPanelMeter.text);
+			if (OriginalPanelMeter == null || PanelMeterText == null) return;
+			PanelMeterText.SetText(OriginalPanelMeter.text);
 		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("OnShipLandedMiscEvents")]
+		private static void OnShipLandedMiscEventsPatch() {
+			Console.LogInfo($"StartOfRound.OnShipLandedMiscEvents() called");
+
+			// //!! DEVELOPMENT ONLY
+			// DisableVisibleTriggers(VisibleTriggerPathsLanded);
+			// Transform originalLight = GameObject.Find("Environment/HangarShip/ShipModels2b/ShipLightsPost/Floodlight1").transform;
+			// if (originalLight != null) {
+			// 	Transform light = GameObject.Instantiate(originalLight, originalLight.parent);
+			// 	light.name = "FloodlightDEV";
+			// 	light.position = new Vector3(-14, 5, -15);
+			// 	light.localEulerAngles = new Vector3(30, 0, 0);
+			// 	Console.LogInfo($"StartOfRound.OnShipLandedMiscEvents() created: {light.name}");
+			// }
+		}
+
+		//? Disable the MeshRenderer on the triggers for better visibility in the hangar for development
+		private static void DisableVisibleTriggers(string[] triggerPaths) {
+			foreach (string triggerPath in triggerPaths) {
+				Transform trigger = GameObject.Find(triggerPath).transform;
+				if (trigger == null) {
+					Console.LogError($"StartOfRound.DisableTrigger() could not find: \n{triggerPath}");
+					continue;
+				}
+				MeshRenderer renderer = trigger.GetComponent<MeshRenderer>();
+				if (renderer != null) {
+					renderer.enabled = false;
+					Console.LogInfo($"Disabled {trigger.name}");
+				}
+				else {
+					if (trigger.childCount > 0) {
+						foreach (Transform child in trigger) {
+							MeshRenderer childRenderer = child.GetComponent<MeshRenderer>();
+							if (childRenderer != null) {
+								childRenderer.enabled = false;
+								Console.LogInfo($"Disabled {trigger.name} > {child.name}");
+							}
+						}
+					}
+					else Console.LogWarning($"StartOfRound.DisableTrigger() could not find MeshRenderer on {trigger.name}");
+				}
+			}
+		}
+
 
 		//_ Monitor Camera buttons: Environment/HangarShip/ShipModels2b/MonitorWall/Cube.001/
 			//- CameraMonitorOnButton
@@ -79,40 +145,72 @@ namespace OuterDoorButtons.Patches
 		}
 
 		//| Transform
-			//> LocalPosition (-0,2 -1,7 0,15)
+			//> Position (-6,36 2,15 -16,23)
+				//- OpenButton
+					//> Scale (-3,5 -3,5 -3,5)
 			//> EulerAngles (90 90 0)
 		private static void CreateMonitorDoorPanel() {
 			if (MonitorWall.Find("MonitorDoorPanel") != null) {
 				Console.LogError($"StartOfRound.CreateMonitorDoorPanel() MonitorDoorPanel already exists");
 				return;
 			}
-			DoorPanel = GameObject.Instantiate(OriginalDoorPanel, MonitorWall);
-			DoorPanel.name = "MonitorDoorPanel";
-			DoorPanel.localPosition = new Vector3(-0.2f, -1.7f, 0.15f);
+			DoorPanel = GameObject.Instantiate(OriginalDoorPanel, OriginalDoorPanel.parent);
+			DoorPanel.name = "OuterDoorPanel";
+			DoorPanel.position = new Vector3(-6.36f, 2.15f, -16.23f);
 			DoorPanel.localEulerAngles = new Vector3(90f, 90f, 0f);
+			DoorPanel.GetComponent<MeshRenderer>().enabled = false;
 			Console.LogInfo($"StartOfRound.CreateMonitorDoorPanel() created: {DoorPanel.name}");
+
+			//? Disable the scan node
+			GameObject scanNode = DoorPanel.Find("ScanNode (2)").gameObject;
+			if (scanNode != null) scanNode.SetActive(false);
+			else Console.LogWarning($"StartOfRound.CreateMonitorDoorPanel() could not find ScanNode (2)");
 
 			//? Get the references for the new buttons
 			OpenButton = DoorPanel.Find("StartButton").Find("Cube (2)");
-			CloseButton = DoorPanel.Find("StopButton").Find("Cube (3)");
+			CloseButton = DoorPanel.Find("StopButton");
 			if (OpenButton == null || CloseButton == null) {
 				Console.LogError($"StartOfRound.CreateMonitorDoorPanel() could not find MonitorDoorPanel references");
 				return;
 			}
+			CloseButton.gameObject.SetActive(false);
+
+			OpenButton.parent.name = "OuterOpenButton";
+			OpenButton.parent.localScale = new Vector3(-3.5f, -3.5f, -3.5f);
+
+			// CloseButton.parent.name = "OuterCloseButton";
+			// CloseButton.parent.localScale = new Vector3(-2f, -2f, -2f);
 
 			OpenButtonTrigger = OpenButton.GetComponent<InteractTrigger>();
-			CloseButtonTrigger = CloseButton.GetComponent<InteractTrigger>();
+			// CloseButtonTrigger = CloseButton.GetComponent<InteractTrigger>();
 
 			//+ Fix button animation and sound by by calling the right trigger on this object
 			OpenButtonTrigger.onInteract.AddListener((player) => {
 				CustomTrigger(player, OriginalOpenButtonTrigger, OpenButton, "Open");
 			});
-			CloseButtonTrigger.onInteract.AddListener((player) => {
-				CustomTrigger(player, OriginalCloseButtonTrigger, CloseButton, "Close");
-			});
+			// CloseButtonTrigger.onInteract.AddListener((player) => {
+			// 	CustomTrigger(player, OriginalCloseButtonTrigger, CloseButton, "Close");
+			// });
 
 			//? find the meter text for later use in UpdatePatch()
-			PanelMeter = DoorPanel.Find("ElevatorPanelScreen/Image/meter").GetComponent<TextMeshProUGUI>();
+			PanelMeter = DoorPanel.Find("ElevatorPanelScreen").transform;
+			if (PanelMeter == null) { Console.LogError($"StartOfRound.CreateMonitorDoorPanel() could not find ElevatorPanelScreen"); return; }
+			PanelMeter.localPosition = new Vector3(0.1f, 0.02f, 0.32f);
+			
+			Transform PanelMeterImage = DoorPanel.Find("ElevatorPanelScreen/Image").transform;
+			if (PanelMeterImage == null) { Console.LogError($"StartOfRound.CreateMonitorDoorPanel() could not find Image on ElevatorPanelScreen"); return; }
+
+			Image panelImage = PanelMeterImage.GetComponent<Image>();
+			if (panelImage != null) panelImage.enabled = false;
+			else Console.LogWarning($"StartOfRound.CreateMonitorDoorPanel() could not find Image on ElevatorPanelScreen");
+
+			GameObject hydra = PanelMeterImage.Find("doorHydraulics").gameObject;
+			if (hydra != null) hydra.SetActive(false);
+			else Console.LogWarning($"StartOfRound.CreateMonitorDoorPanel() could not find doorHydraulics");
+
+			PanelMeterText = PanelMeterImage.Find("meter").GetComponent<TextMeshProUGUI>();
+			PanelMeterText.alignment = TextAlignmentOptions.Top;
+			PanelMeterText.horizontalAlignment = HorizontalAlignmentOptions.Center;
 		}
 
 		private static void CustomTrigger(PlayerControllerB sender, InteractTrigger originalTrigger, Transform trigger, string state = "Open") {
